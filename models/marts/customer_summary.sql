68b6d5eb5e904703529111dd669e95e29a869242
ramya-rl-alation <ramya.rajalakshmi@alation.com>
2026-08-13 20:52:38 +0530

    Update customer_summary.sql

--- models/marts/customer_summary.sql
+++ models/marts/customer_summary.sql
@@ -1,8 +1,6 @@
 SELECT
-    c.customer_id,
-    c.customer_name,
-    COUNT(o.order_id) AS total_orders,
-    SUM(o.amount) AS total_spent
+    c.id AS customer_id,
+    COUNT(o.id) AS total_orders
 FROM {{ ref('stg_customers') }} c
-LEFT JOIN {{ ref('stg_orders') }} o ON c.customer_id = o.customer_id
-GROUP BY 1, 2
+LEFT JOIN {{ ref('stg_orders') }} o ON c.id = o.customer_id
+GROUP BY 1
